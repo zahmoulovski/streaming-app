1e00082ce763f44e6547d203deb39bf9dcdbbf85
zahmoulovski <medyassine.zahmoul@gmail.com>
2022-12-09 06:23:36 +0100

    iframe editing

--- src/Components/Streaming.js
+++ src/Components/Streaming.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Streaming = ({code})=>(
   <div className="video-responsive">
-    <iframe src={`https://sblongvu.com/e/${code}.html`} width="100%" height="80%" allowFullScreen/>
+    <iframe src={`https://sblongvu.com/e/${code}.html`} width="100%" height="80%" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
   </div>
 );
 
